d31b78d972f11615b52f81b0599a830d68fca441
Daniel <daniel.zhou@telecom-paris.fr>
2023-03-31 10:03:56 +0200

    better looking code for tasksListing

--- src/components/tasksListing.tsx
+++ src/components/tasksListing.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Task } from '../task';
+import {Task} from '../task';
 
 function getTasks(): Task[] { // Hard-coded for now
     return [
-        { title: 'Task 1', details: 'Task 1 description', isComplete: false, date: new Date('2021-01-01') },
-        { title: 'Task 2', details: 'Task 2 description', isComplete: true, date: new Date('2021-01-03'), location: 'Paris', sharedWith: ['John'] },
+        {
+            title: 'Task 1',
+            details: 'Task 1 description',
+            isComplete: false,
+            date: new Date('2021-01-01')
+        },
+        {
+            title: 'Task 2',
+            details: 'Task 2 description',
+            isComplete: true,
+            date: new Date('2021-01-03'),
+            location: 'Paris',
+            sharedWith: ['John']
+        },
         {
             title: 'Task 3',
             details: 'Task 3 description',
@@ -24,10 +36,10 @@ function displayTasks() {
             {
                 tasks.map((task: any, index: any) => {
                     return (
-                        <div key={index} style={{ borderRadius: '20px' }}>
-                            <input type={'checkbox'} checked={task.isComplete} />
+                        <div key={index} style={{borderRadius: '20px'}}>
+                            <input type={'checkbox'} checked={task.isComplete}/>
                             <h3>{task.title}</h3>
-                            {task.details && <p style={{ color: 'grey' }}>{task.details}</p>}
+                            {task.details && <p style={{color: 'grey'}}>{task.details}</p>}
                             {task.date && <p>{task.date}</p>}
                             {task.location && <p>{task.location}</p>}
                             {task.sharedWith && <p>{task.sharedWith.join(', ')}</p>}
