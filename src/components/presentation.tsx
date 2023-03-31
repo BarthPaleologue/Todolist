71cc8ff87557f5d35c386ff74af51666ab909286
adrienctl <adrien.coutelle@telecom-paris.fr>
2023-03-31 10:53:13 +0200

    premier test

---
+++ src/components/presentation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function presentation()
+{
+    return (
+        <div className = 'verticalView'>
+            <h1>How to use the App </h1>
+            <img src = {require('../../public/logo.png')} />
+            <li></li>
+        </div>
+    );
+}
