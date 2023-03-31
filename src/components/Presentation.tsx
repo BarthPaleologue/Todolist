08640c80ecc2f5d7a14a6265c6ca5239aad12a13
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-03-31 11:33:29 +0200

    added header to presentation

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 import logo from '../assets/logo.png';
 
 
-export function Presentation({onOK}: { onOK: () => void}) {
+export function Presentation({ onOK }: { onOK: () => void; }) {
     return (
         <div className='verticalView'>
-            <h1>How to use the App </h1>
-            <img src = {logo} width="15%"/>
+            <header>
+                <h1>How to use the App </h1>
+            </header>
+            <img src={logo} width="15%" />
             <li>Click on the "+" button at the button to add a category</li>
             <li>Click on a category to display its tasks</li>
             <li>Click on your photo to edit your profile</li>
             <li>Click on the "..." button to edit a category</li>
-            <button onClick = {onOK} style={{width: '50px'}}> OK </button>
+            <button onClick={onOK} style={{ width: '50px' }}> OK </button>
         </div>
     );
 }
