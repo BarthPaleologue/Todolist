a9fcae87aa0ae14bd11088d1b9b96904e45047ae
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-04-12 16:46:08 +0200

    centered presentation text

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -9,16 +9,15 @@ export function Presentation({ onOK }: { onOK: () => void }) {
             <Header title="Welcome to Todos!" shouldHideBackButton={true} />
             <section className="mainContainer">
                 <img className="logo" src={logo} />
-                <ul className="listContainer">
-                    <li>Click on the "+" button at the button to add a category</li>
-                    <li>Click on a category to display its tasks</li>
-                    <li>Click on your photo to edit your profile</li>
-                    <li>Click on the "..." button to edit a category</li>
+                <ul className="listContainer presentation">
+                    <li>Manage your tasks easily!</li>
+                    <li>To get started, press "Let's go!" then press "New Task". It's that easy</li>
+                    <li>You can edit a task by clicking on it.</li>
                 </ul>
             </section>
             <div className="buttonBlock">
                 <button onClick={onOK} style={{ width: "50px" }}>
-                    Accept
+                    Let's go!
                 </button>
             </div>
         </div>
