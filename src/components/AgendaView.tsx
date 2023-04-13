81df0906f39de0cb8278b33194566e7fd60e9c5f
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-04-13 15:20:30 +0200

    busy days are now highlighted

--- src/components/AgendaView.tsx
+++ src/components/AgendaView.tsx
@@ -1,18 +1,40 @@
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { Header } from "./Header";
+import { useState } from "react";
+import { loadTasksFromLocalStorage } from "../utils/localStorage";
 
 interface AgendaViewProps {
     onDateChange: (date: Date) => void;
 }
 
 export function AgendaView({ onDateChange }: AgendaViewProps) {
+    const [currentDate, setCurrentDate] = useState<Date>(new Date());
+
+    const tasks = loadTasksFromLocalStorage();
+
+    function isThereATaskOnDate(date: Date) {
+        for (const task of tasks) {
+            if (task.date === undefined) continue;
+            if (task.date.getDate() === date.getDate() && task.date.getMonth() === date.getMonth() && task.date.getFullYear() === date.getFullYear()) return true;
+        }
+        return false;
+    }
+
     return (
         <div className="verticalView">
             <Header title="Agenda" shouldHideBackButton={true} />
 
             <div className="mainContainer agendaContainer">
-                <Calendar locale="en-GB" onChange={(value) => onDateChange(value as Date)} />
+                <Calendar
+                    tileClassName={({ activeStartDate, date, view }) => (view === "month" && isThereATaskOnDate(date) ? "busyDay" : null)}
+                    locale="en-GB"
+                    value={currentDate}
+                    onChange={(value) => {
+                        onDateChange(value as Date);
+                        setCurrentDate(value as Date);
+                    }}
+                />
             </div>
         </div>
     );
